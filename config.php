344ab173624087050976fca7f5d140adb7a4bbed
rachelliu1020 <rachelliu1020@gmail.com>
2017-04-24 02:12:10 -0700

    put css files into css folder

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
     define('DB_USER', 'racliu_game');
     define('DB_PASSWORD', '10200308xkx');
     define('DB_DATABASE', 'racliu_game');
-    define('HOMEURL', 'http://www.rachelliu1020.net76.net/forum/index.php');
+    define('HOMEURL', 'http://www.magician.we.bs/magic_card/homepage.php');
 ?>
 
--- leaderboard.php
+++ leaderboard.php
@@ -10,7 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Magic Cards/leaderboard</title>
-    <link rel="stylesheet" href="game.css" media="only all and (min-width:320px)">
+    <link rel="stylesheet" href="css/game.css" media="only all and (min-width:320px)">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <script src="js/leaderboard.js"></script>
